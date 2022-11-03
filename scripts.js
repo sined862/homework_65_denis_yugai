a888fddcdea8f1dd06f4dd046a0390c1d6afce9e
Yugai Denis <yugai_denis@mail.ru>
2022-11-03 13:49:29 +0600

    Написание второго задания в scripts.js

--- scripts.js
+++ scripts.js
@@ -9,3 +9,21 @@ for (let i=0; i < listProducts.length; i++) {
         console.log('плохо')
     }
 }
+
+//Задание 2
+const arrayNumbers1 = [1, 5, 12, 4, 3]
+const arrayNumbers2 = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+const arrayNumbers3 = [3, 10, 17]
+
+function avgNumbers(anyArray) {
+    let sumNumbers = 0
+    for (i=0; i < anyArray.length; i++) {
+        sumNumbers += anyArray[i]
+    }
+    let avgNumbers = sumNumbers / anyArray.length
+    return avgNumbers
+}
+
+console.log(avgNumbers(arrayNumbers1))
+console.log(avgNumbers(arrayNumbers2))
+console.log(avgNumbers(arrayNumbers3))
