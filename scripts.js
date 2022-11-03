c17bed973b32b052b5107c1e029f323ef01730a4
Yugai Denis <yugai_denis@mail.ru>
2022-11-03 13:47:29 +0600

    Написание первого задания в scripts.js + подключение файла scripts.js на странице index.html

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
-﻿
+﻿//Задание 1
+const listProducts = ['молоко', 'пиво', 'пиво', 'молоко', 'молоко']
+
+for (let i=0; i < listProducts.length; i++) {
+    if (listProducts[i] == 'молоко') {
+        console.log('хорошо')
+    }
+    else {
+        console.log('плохо')
+    }
+}
